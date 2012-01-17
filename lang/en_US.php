7125fec14106b7d2a13e0a15d0e00e2744b3944a
Pali Ondras <admin@silverstripe.sk>
2012-01-17 22:37:09 +0100

    added lang files

---
+++ lang/en_US.php
@@ -0,0 +1,13 @@
+<?php
+
+// Disqus
+$lang['en_US']['Disqus']['COMMENTSHEADING-JSOFF'] = 
+	'Comments';
+$lang['en_US']['Disqus']['NOCOMMENTS-JSOFF'] = 
+	'No comments on this article.';
+$lang['en_US']['Disqus']['SYNCCOMMENTSBUTTON'] = 
+	'Sync Disqus Comments';
+$lang['en_US']['Disqus']['COMMENTS'] = 
+	'Comments';
+$lang['en_US']['Disqus']['syncAllCommentsActionButton'] = 
+	'Sync all Disqus comments';
